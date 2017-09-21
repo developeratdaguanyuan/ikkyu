6c43229b430cd88571bcee077ffddbf62213e642
developeratdaguanyuan <developeratdaguanyuan@gmail.com>
2017-09-20 23:21:07 -0500

    add upload relation vectors and relation id map to Redis

--- cfo/builddata/src/main/java/ikkyu/cfo/redisclient/DataUploader.java
+++ cfo/builddata/src/main/java/ikkyu/cfo/redisclient/DataUploader.java
@@ -5,21 +5,19 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import edu.stanford.nlp.util.StringUtils;
 import nlp.tokenizer.EnglishTokenizer;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 public class DataUploader {
-  public static void UploadEntityMID2NameOrID(String path, int db_id) {
+  public static void UploadEntityMID2ID(String path, int db_id) {
     Jedis jedis = null;
     JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost");
     try {
@@ -46,10 +44,50 @@ public class DataUploader {
       }
       reader.close();
     } catch (FileNotFoundException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     } catch (IOException e) {
-      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } finally {
+      if (jedis != null) {
+        jedis.close();
+      }
+    }
+    pool.destroy();
+  }
+
+  public static void UploadEntityMID2Name(String path, int db_id, int check_db_id) {
+    Jedis jedis = null;
+    JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost");
+    try {
+      jedis = pool.getResource();
+      String line;
+      File fin = new File(path);
+      BufferedReader reader = new BufferedReader(new FileReader(fin));
+      while ((line = reader.readLine()) != null) {
+        String[] tokens = line.trim().split("\t");
+        if (tokens.length < 2) {
+          continue;
+        }
+        String subject_mid = tokens[0].trim().replace("www.freebase.com", "");
+        jedis.select(check_db_id);
+        if (!jedis.exists(subject_mid)) {
+          continue;
+        }
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; i < tokens.length; i++) {
+          builder.append(tokens[i]).append(" ");
+        }
+        String value = builder.toString().trim();
+        if (value.length() > 2 && value.charAt(0) == '\"' && value.charAt(value.length() - 1) == '\"') {
+          value = value.substring(1, value.length() - 1);
+        }
+        jedis.select(db_id);
+        jedis.set(subject_mid, value);
+      }
+      reader.close();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
       e.printStackTrace();
     } finally {
       if (jedis != null) {
@@ -74,10 +112,8 @@ public class DataUploader {
       }
       reader.close();
     } catch (FileNotFoundException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     } catch (IOException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     } finally {
       if (jedis != null) {
@@ -87,7 +123,7 @@ public class DataUploader {
     pool.destroy();
   }
 
-  public static void UploadEntityname2MIDs(String path, int db_id) {
+  public static void UploadEntityname2MIDs(String path, int db_id, int check_db_id) {
     Jedis jedis = null;
     JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost");
     try {
@@ -113,6 +149,11 @@ public class DataUploader {
         name = name_tokens.stream().collect(Collectors.joining(" "));
 
         String mid = tokens[0].replace("www.freebase.com", "");
+        jedis.select(check_db_id);
+        if (!jedis.exists(mid)) {
+          continue;
+        }
+        jedis.select(db_id);
         if (jedis.exists(name)) {
           Set<String> set = new HashSet<String>(Arrays.asList(jedis.get(name).split(";")));
           if (!set.contains(mid)) {
@@ -125,10 +166,8 @@ public class DataUploader {
       }
       reader.close();
     } catch (FileNotFoundException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     } catch (IOException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     } finally {
       if (jedis != null) {
@@ -138,11 +177,43 @@ public class DataUploader {
     pool.destroy();
   }
 
+  public static void UploadEntityMID2Name(String path, int db_id) {
+    Jedis jedis = null;
+    JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost");
+    try {
+      jedis = pool.getResource();
+      jedis.select(db_id);
+      String line;
+      File fin = new File(path);
+      BufferedReader reader = new BufferedReader(new FileReader(fin));
+      while ((line = reader.readLine()) != null) {
+        String[] tokens = line.trim().split("\t");
+        if (tokens.length < 2) {
+          continue;
+        }
+        String relation_mid = tokens[0].trim().replace("www.freebase.com", "");
+        String relation_id = tokens[1].trim();
+        jedis.set(relation_id, relation_mid);
+      }
+      reader.close();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      if (jedis != null) {
+        jedis.close();
+      }
+    }
+    pool.destroy();
+  }
 
   public final static void main(String[] args) {
-    UploadEntityMID2NameOrID("../data/transE/FB5M-entity-id.txt", 0);
-    UploadEntityMID2NameOrID("../../data/entity_name_map.txt", 1);
-    UploadEntityMID2Embeddings("../data/transE/entity_embeddings.txt", 2);
-    UploadEntityname2MIDs("../../data/entity_name_map.txt", 3);
+    UploadEntityMID2ID("../data/transE/FB5M-entity-id.txt", 0);
+    UploadEntityMID2Name("../../data/entity_name_map.txt", 1, 0);
+    UploadEntityname2MIDs("../../data/entity_name_map.txt", 2, 0);
+    UploadEntityMID2Embeddings("../data/transE/entity_embeddings.txt", 3);
+    UploadEntityMID2Name("../data/transE/FB5M-relation-id.txt", 4);
+    UploadEntityMID2ID("../../data/FB5M-relation-id-tiny.txt", 5);
   }
 }
